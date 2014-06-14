53b2798b45c75bd85ad98ce4bc66d496e7a606d0
x <x@x.x>
2014-06-14 11:10:16 -0400

    nice clean-up, bro

--- build/CMakeFiles/apng2mng.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for apng2mng.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/Soft/Converters/apng2mng
-# Build directory: /home/Soft/Converters/apng2mng/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(apng2mng "/home/Soft/Converters/apng2mng/build/apng2mng")
